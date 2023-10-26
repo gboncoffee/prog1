4c395c0808f81fdb431382cf2f49c8ca1cb3d44c
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-10-25 22:06:45 -0300

    Terminado conjuntos

--- conjunto/conjunto.c
+++ conjunto/conjunto.c
@@ -10,16 +10,15 @@
 
 struct conjunto *cria_cjt(int max)
 {
-    int *v;
     struct conjunto *novo;
 
-    /* colocar "v = ..." embaixo continua aproveitando o shortcut e fica mais
-     * legível por não ficar longo */
-    v = malloc(sizeof(int) * max);
-    if (!(v && (novo = malloc(sizeof(struct conjunto)))))
+    if (!(novo = malloc(sizeof(struct conjunto))))
         return NULL;
+    if (!(novo->v = malloc(sizeof(int) * max))) {
+        free(novo);
+        return NULL;
+    }
 
-    novo->v = v;
     novo->max = max;
     novo->card = 0;
     novo->ptr = 0;
@@ -52,29 +51,35 @@ int cardinalidade_cjt(struct conjunto *c)
     return c->card;
 }
 
-/* TODO: arrumar busca */
-
 /* serve para varias operacoes, retorna a posicao em pos. o valor de retorno eh
  * 1 em sucesso. nao verifica NULL, o chamador deve verificar
  *
  * pos nao pode ser NULL de jeito nenhum pois sempre mexe-se nela */
 int busca_binaria(struct conjunto *c, int elemento, int ini, int fim, int *pos)
 {
-    if (ini > fim)
-        return 0;
-    if (ini == fim) {
+    /* precisamos garantir que *pos tera a posicao exata em que precisamos 
+     * inserir um elemento se necessario */
+    if (ini > fim) {
         *pos = ini;
-        return c->v[ini] == elemento;
+        return 0;
+    }
+    if (ini == fim) {
+        if (c->v[ini] == elemento) {
+            *pos = ini;
+            return 1;
+        }
+        if (c->v[ini] > elemento)
+            *pos = ini;
+        else
+            *pos = ini + 1;
+        return 0;
     }
 
-    /* usa a pos pois assim na saida 0 aproveita-se a pos setada
-     * anteriormente */
-    *pos = (fim - ini) / 2 + 1;
+    *pos = (fim - ini) / 2 + ini;
     if (c->v[*pos] > elemento)
         return busca_binaria(c, elemento, ini, *pos - 1, pos);
-    else if (c->v[*pos] < elemento)
+    if (c->v[*pos] < elemento)
         return busca_binaria(c, elemento, *pos + 1, fim, pos);
-
     return 1;
 }
 
@@ -102,25 +107,25 @@ void insere_pos(struct conjunto *c, int elemento, int pos, int mov)
 
 int insere_cjt(struct conjunto *c, int elemento)
 {
-    int pos;
+    int pos = 0;
     if (c == NULL || c->card == c->max)
         return 0;
     if (BUSCA(c, elemento, &pos))
         return 1;
 
-    insere_pos(c, elemento, pos, c->card - 1);
+    insere_pos(c, elemento, pos, c->card);
     c->card++;
 
     return 1;
 }
 
 /* auxiliar para retira_cjt semelhante a insere_pos */
-void retira_pos(struct conjunto *c, int pos, int mov)
+void retira_pos(struct conjunto *c, int pos, int fim)
 {
-    if (pos == mov)
+    if (pos > fim)
         return;
-    c->v[mov - 1] = c->v[mov];
-    retira_pos(c, pos, mov - 1);
+    c->v[pos] = c->v[pos + 1];
+    retira_pos(c, pos + 1, fim);
 }
 
 int retira_cjt(struct conjunto *c, int elemento)
@@ -130,7 +135,7 @@ int retira_cjt(struct conjunto *c, int elemento)
         return 0;
 
     c->card--;
-    retira_pos(c, pos, c->card - 1);
+    retira_pos(c, pos, c->card);
     return 1;
 }
 
@@ -155,7 +160,8 @@ int contido_cjt(struct conjunto *c1, struct conjunto *c2)
     return contido(c1, c2, 0, 0);
 }
 
-/* compara dois conjuntos de mesma cardinalidade, auxiliar para sao_iguais_cjt */
+/* compara dois conjuntos de mesma cardinalidade, auxiliar para
+ * sao_iguais_cjt */
 int cmpcjt(struct conjunto *c1, struct conjunto *c2, int p)
 {
     if (c1->card == p)
@@ -172,42 +178,32 @@ int sao_iguais_cjt(struct conjunto *c1, struct conjunto *c2)
     return cmpcjt(c1, c2, 0);
 }
 
-/* basicamente, p1 e p2 sao indices em c1 e c2 que demonstram qual elemento
- * devemos computar. se c2[p2] > c1[p1], significa que nao ha c1[p1] em c2 e
- * podemos adiciona-lo ao resultado. isso so eh possivel pois os conjuntos estao
- * ordenados.
- *
- * se c2[p2] < c1[p1], precisamos continuar verificando em c2 e ainda nao
- * podemos adicionar c1[p1] ao resultado. se sao iguais, passamos para o proximo
- * elemento de cada. */
+/* os algoritmos que computam diferenca, uniao e interseccao sao bem parecidos.
+ * ambos iteram os dois conjuntos aumentando o ponteiro de cada um quando o
+ * elemento do outro for maior. */
 void computa_diff(struct conjunto *c1,
                   struct conjunto *c2,
                   struct conjunto *cr,
                   int p1,
                   int p2)
 {
-    if (p1 == c1->card)
+    int novo_p1 = p1;
+    int novo_p2 = p2;
+
+    if (p1 >= c1->card)
         return;
 
-    if (p2 < c2->card) {
-        /* nao ha c1[p1] em c2 entao adicionamos ao resultado */
-        if (c2->v[p2] > c1->v[p1]) {
-            cr->v[cr->card] = c1->v[p1];
-            cr->card++;
-        }
-
-        if (c2->v[p2] >= c1->v[p1])
-            p1++;
-        if (c2->v[p2] <= c1->v[p1])
-            p2++;
-    } else {
-        /* so roda quando chegar ao final de c2, basicamente copia tudo que
-         * resta de c1 para o resultado */
+    if (p2 >= c2->card || c1->v[p1] < c2->v[p2]) {
         cr->v[cr->card] = c1->v[p1];
-        p1++;
         cr->card++;
+        novo_p1++;
+    } else {
+        if (c1->v[p1] == c2->v[p2])
+            novo_p1++;
+        novo_p2++;
     }
-    computa_diff(c1, c2, cr, p1, p2);
+
+    computa_diff(c1, c2, cr, novo_p1, novo_p2);
 }
 
 struct conjunto *diferenca_cjt(struct conjunto *c1, struct conjunto *c2)
@@ -225,17 +221,22 @@ void computa_intc(struct conjunto *c1,
                   int p1,
                   int p2)
 {
+    int novo_p2 = p2;
+    int novo_p1 = p1;
     if (p1 >= c1->card || p2 >= c2->card)
         return;
-    if (c1->v[p1] > c2->v[p2])
-        p2++;
-    if (c1->v[p1] < c2->v[p2])
-        p1++;
+
     if (c1->v[p1] == c2->v[p2]) {
         cr->v[cr->card] = c1->v[p1];
         cr->card++;
     }
-    computa_intc(c1, c2, cr, p1, p2);
+
+    if (c1->v[p1] >= c2->v[p2])
+        novo_p2++;
+    if (c1->v[p1] <= c2->v[p2])
+        novo_p1++;
+
+    computa_intc(c1, c2, cr, novo_p1, novo_p2);
 }
 
 struct conjunto *interseccao_cjt(struct conjunto *c1, struct conjunto *c2)
@@ -247,10 +248,6 @@ struct conjunto *interseccao_cjt(struct conjunto *c1, struct conjunto *c2)
     return intc;
 }
 
-/* bem parecido com o diff
- *
- * adiciona c1[p1] caso seja menor ou igual a c2[p2], incrementa ambos caso
- * sejam <= o outro */
 void computa_uniao(struct conjunto *c1,
                    struct conjunto *c2,
                    struct conjunto *cr,
@@ -259,16 +256,17 @@ void computa_uniao(struct conjunto *c1,
 {
     if (p1 >= c1->card && p2 >= c2->card)
         return;
-    if (p1 < c1->card && p2 < c2->card) {
-        if (c1->v[p1] > c2->v[p2]) {
-            cr->v[cr->card] = c2->v[p2];
-        } else {
-            cr->v[cr->card] = c1->v[p1];
-            p1++;
-        }
 
-        if (c1->v[p1] >= c2->v[p2])
-            p2++;
+    if (p1 >= c1->card || (c1->v[p1] > c2->v[p2])) {
+        cr->v[cr->card] = c2->v[p2];
+        p2++;
+    } else if (p2 >= c2->card || (c2->v[p2] > c1->v[p1])) {
+        cr->v[cr->card] = c1->v[p1];
+        p1++;
+    } else {
+        cr->v[cr->card] = c1->v[p1];
+        p1++;
+        p2++;
     }
 
     cr->card++;
@@ -298,13 +296,21 @@ struct conjunto *copia_cjt(struct conjunto *c)
     if (novo == NULL)
         return NULL;
     copia(novo, c, 0);
+    novo->card = c->card;
+
     return novo;
 }
 
 int retira_um_elemento_cjt(struct conjunto *c)
 {
-    int r = rand() % c->card;
-    int n = c->v[r];
+    int r, n;
+    if (vazio_cjt(c))
+        return 0;
+    if (c->card == 1)
+        r = 0;
+    else
+        r = rand() % (c->card - 1);
+    n = c->v[r];
     retira_pos(c, r, c->card - 1);
     c->card--;
     return n;
@@ -312,17 +318,22 @@ int retira_um_elemento_cjt(struct conjunto *c)
 
 struct conjunto *cria_subcjt_cjt(struct conjunto *c, int n)
 {
-    struct conjunto *novo;
     int i;
-    if (vazio_cjt(c))
-        return cria_cjt(n);
+    struct conjunto *copia;
+    struct conjunto *novo;
+
+    if (n == 0 || vazio_cjt(c))
+        return cria_cjt(c->max);
+
+    copia = copia_cjt(c);
     if (n >= c->card)
-        return copia_cjt(c);
+        return copia;
 
-    novo = cria_cjt(n);
-
+    novo = cria_cjt(c->card);
     for (i = 0; i < n; i++)
-        insere_cjt(novo, retira_um_elemento_cjt(c));
+        insere_cjt(novo, retira_um_elemento_cjt(copia));
+
+    destroi_cjt(copia);
 
     return novo;
 }
@@ -330,7 +341,11 @@ struct conjunto *cria_subcjt_cjt(struct conjunto *c, int n)
 void imprime_cjt(struct conjunto *c)
 {
     int i;
-    for (i = 0; i < c->card - 2; i++)
+    if (c->card == 0) {
+        printf("conjunto vazio\n");
+        return;
+    }
+    for (i = 0; i < c->card - 1; i++)
         printf("%d ", c->v[i]);
     printf("%d\n", c->v[c->card - 1]);
 }
@@ -342,7 +357,9 @@ void inicia_iterador_cjt(struct conjunto *c)
 
 int incrementa_iterador_cjt(struct conjunto *c, int *ret_iterador)
 {
+    if (c->ptr >= c->card)
+        return 0;
     *ret_iterador = c->v[c->ptr];
     c->ptr++;
-    return c->ptr < c->card;
+    return 1;
 }
